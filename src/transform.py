f6783c44fbc809e196c535fe1a87b01f7d51c685
jaime <164238589+jvime13@users.noreply.github.com>
2025-09-24 19:10:20 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,9 @@
+# Script de normalización y transformación
+# Genera capas bronze y silver
+
+def transform_data(input_path, output_path="../data/bronze/"):
+    # TODO: implementar transformaciones
+    pass
+
+if __name__ == "__main__":
+    print("Transformación ejecutada.")
