c73ad7216ac819480cae8850d673e136efe0b8f0
jaime <164238589+jvime13@users.noreply.github.com>
2025-09-24 19:07:42 +0200

    Create ingest.py

---
+++ src/ingest.py
@@ -0,0 +1,9 @@
+# Script de ingesta de CSVs heterogéneos
+# Guarda los archivos crudos en la carpeta /data/raw
+
+def ingest_csv(file_path, output_path="../data/raw/"):
+    # TODO: implementar lógica de copia/almacenamiento
+    pass
+
+if __name__ == "__main__":
+    print("Ingesta ejecutada.")
