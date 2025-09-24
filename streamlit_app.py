68dcd8d177ce4ad766c0c1d401dcb6421be5c874
jaime <164238589+jvime13@users.noreply.github.com>
2025-09-24 19:06:30 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+# Streamlit app de visualización de KPIs
+import streamlit as st
+
+st.title("📊 KPIs desde la capa Silver")
+st.write("App inicial en Streamlit. Conéctala con tus datos transformados.")
